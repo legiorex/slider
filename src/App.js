8bb26b2f6faff707615b6a3164cba7171ceff989
Dima Vakatsiienko <dvakatsiienko@lectrum.io>
2018-11-27 00:00:54 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,73 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+import image1 from './assets/crs.jpg';
+import image2 from './assets/jellyfish.jpg';
+import image3 from './assets/observatory.jpg';
+
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+    state = {
+        images: [
+            // error
+            { id: '123', src: image1 }, // 0
+            { id: '456', src: image2 }, // 1 ←
+            { id: '678', src: image3 }, // 2
+        ],
+        selectedImageId: '456', // ←
+        // selectedImageIndex: 1,
+    };
+
+    _selectPrevImage = () => {
+        const { images, selectedImageId } = this.state;
+
+        const selectedImageIndex = images.findIndex(
+            image => image.id === selectedImageId, // предикат (predicate)
+        );
+
+        let prevImageIndex = selectedImageIndex - 1;
+
+        // if (prevImageIndex === -1) {
+        //     return null;
+        // }
+
+        if (prevImageIndex === -1) {
+            prevImageIndex = images.length - 1;
+        }
+
+        const { id } = images.find((image, index) => index === prevImageIndex);
+
+        this.setState({
+            selectedImageId: id,
+        });
+
+        // 1. перебрать images
+        // 2. сравнить каждый элемент с selectedImageId
+        // 3. выбрать предыдущий
+    };
+
+    render() {
+        const { images, selectedImageId } = this.state;
+
+        // Через id
+        const selectedImage = images.find(
+            image => image.id === selectedImageId,
+        );
+
+        // Через index
+        // const selectedImage = images.find(
+        //     (image, index) => index === selectedImageId,
+        // );
+
+        return (
+            <div className="app">
+                <img src={selectedImage.src} />
+                <div className="control">
+                    <button onClick={this._selectPrevImage}>←</button>
+                    <button>→</button>
+                </div>
+            </div>
+        );
+    }
 }
 
 export default App;
