c0825fb5ce00d0b91e3535d84dd93c70c76e413b
legiorex <batyushev@gmail.com>
2018-11-27 22:49:26 +1000

    сделал загрузку с сервера

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
+import axios from 'axios';
 
 import image1 from './assets/crs.jpg';
 import image2 from './assets/jellyfish.jpg';
@@ -9,13 +10,27 @@ class App extends Component {
   state = {
     images: [
       // error
-      { id: "123", src: image1 }, // 0
+      { id: "123", 
+      src: image1,
+    author: ''
+ }, // 0
       { id: "456", src: image2 }, // 1 ←
       { id: "678", src: image3 } // 2
     ],
     selectedImageId: "456" // ←
     // selectedImageIndex: 1,
   };
+
+  componentDidMount(){
+      axios
+          .get("http://5bf26792a60fe600134cdf1a.mockapi.io/photoArray")
+          .then(({ data }) => {
+              console.log(data);
+          });
+  }
+
+  
+    
 
   _selectPrevImage = () => {
     const { images, selectedImageId } = this.state;
@@ -44,30 +59,28 @@ class App extends Component {
     // 2. сравнить каждый элемент с selectedImageId
     // 3. выбрать предыдущий
   };
-
+    
   _nextPrevImage = () => {
-      
-      const { images, selectedImageId } = this.state;
-      const selectedImageIndex = images.findIndex(
-          image => image.id === selectedImageId 
-      );
+    const { images, selectedImageId } = this.state;
+    const selectedImageIndex = images.findIndex(
+      image => image.id === selectedImageId
+    );
 
-      let nextImageIndex = selectedImageIndex + 1;
-      
-      if (nextImageIndex === images.length) {
-        nextImageIndex = 0;
-      }
+    let nextImageIndex = selectedImageIndex + 1;
 
-      const { id } = images.find((image, index) => index === nextImageIndex);
+    if (nextImageIndex === images.length) {
+      nextImageIndex = 0;
+    }
 
-      this.setState({
-          selectedImageId: id
-      });
+    const { id } = images.find((image, index) => index === nextImageIndex);
 
-
-  }
+    this.setState({
+      selectedImageId: id
+    });
+  };
 
   render() {
+      
     const { images, selectedImageId } = this.state;
 
     // Через id
