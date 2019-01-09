a28e7493cfa0187135bec843583fdd0ae018ee80
legiorex <batyushev@gmail.com>
2019-01-09 21:40:14 +1000

    непрерывный скролл 2

--- src/Wall.js
+++ src/Wall.js
@@ -4,26 +4,28 @@ import './Wall.css';
 import PagePhoto from './PagePhoto';
 
 class Wall extends Component {
-  // componentDidMount() {
-  //   this._fetchPhotoAsync();
-  // }
-
-  // _fetchPhotoAsync = async () => {
-  //   const photoArray = await api.fetchPhotos();
-
-  //   this.setState({ photoArray });
-  // };
+  componentDidMount() {
+    this._addNewPage();
+  }
+  
 
   state = {
-    pages: [0],
-    addPage: false,
+    pages: [],
+    
   };
+
+  _addNewPage = () => {
+    const { pages } = this.state;
+    this.setState({ pages: pages.concat(this._idGen())});
+    // console.log(this.state.pages)
+  }
 
   _scrollEvent = () => {
     document.body.onwheel = event => {
       event.preventDefault();
 
-      this._test(event);
+      this._checkScroll(event);
+      
 
       if (event.deltaY > 0) {
         window.scrollBy(100, 0);
@@ -33,17 +35,27 @@ class Wall extends Component {
     };
   };
 
-  _test = event => {
+  _checkScroll = event => {
+    const { pages } = this.state;
+
     const contentWrapper = event.target.parentNode.parentNode;
-
     const endWindow = contentWrapper.scrollWidth - document.body.clientWidth;
+    
+   
+    // console.log('ширина блока   ', endWindow - 100);
+    // console.log("текущий скролл   ", window.pageXOffset);
 
-    if (endWindow <= window.pageXOffset) {
-      console.log('true');
-      this.setState({ addPage: true });
+    if (endWindow <= window.pageXOffset && pages.length < 2) {
+      this._addNewPage();
+      console.log("true");
+      // this.setState({ pages: pages.concat(1) });
     } else {
-      console.log('false');
+      console.log("false");
     }
+  };
+
+  _idGen = () => {
+    return Math.random().toString(36).substring(2, 7)
   };
 
   render() {
